f72d1144f9b05715a985c98194ddac3ff257320f
Vivek Kumar <vvek.kr@gmail.com>
2018-07-14 20:12:27 -0700

    inc 0714(2)

--- dsp_1/sig_1/exercises/exM2_1_til_M2_4.py
+++ dsp_1/sig_1/exercises/exM2_1_til_M2_4.py
@@ -48,6 +48,34 @@ def ramp(n_init, L, Fs, is_plot = True):
          fig = plotter(n, seq, 'ramp')
     return seq
 
+<<<<<<< HEAD
+def prdc_square(n_init, pk_val, L, duty_cycle_pc, T, Fs, is_plot = True):
+    n = np.arange(n_init, L*1/Fs, 1/Fs)
+    seq = [ pk_val*1 if (idx % T) <= duty_cycle_pc*T else -1*pk_val for idx, i in enumerate(n) ]
+    if is_plot is True :
+         fig = plotter(n, seq, 'ramp')
+    return seq    
+
+
+def prdc_sawtooth(n_init, pk_val, L, T, Fs, is_plot = True):
+    
+    seq0 = [ pk_val * (idx % T)/T for idx in range(L) ]
+    seq = [ val - (pk_val /2) for val in seq0 ]
+    if is_plot is True :
+         fig = plotter(range(L), seq, 'sawtooth')
+    return seq    
+
+# Test
+n_init = 0
+L = 100
+Fs = 20000
+pk_val = 7
+T = 13
+
+duty_cycle_pc = .6
+# prdc_square(n_init, pk_val, L, duty_cycle_pc, T, Fs)
+x5 = prdc_sawtooth(n_init, pk_val, L, T, Fs)
+=======
 # Test
 # n = range(-20, 40)
 
@@ -60,3 +88,4 @@ Fs = 20000
 x = dirac(n_init, L, Fs)
 x2 = step(n_init, L, Fs)
 x3 = ramp(n_init, L, Fs)
+>>>>>>> 593eaee93cb26dd4a4c82e354d643c1ae00ba9e5
