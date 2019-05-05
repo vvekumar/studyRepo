9127ba83f782bb4e8ffe70340e874890ee713250
Vivek Kumar <vvek.kr@gmail.com>
2019-05-04 23:05:32 -0700

    Adding initial solution to problem 81

--- euler/eid_081.py
+++ euler/eid_081.py
@@ -3,9 +3,64 @@ Problem ID : 81
 link : https://projecteuler.net/problem=81
 """
 
-def design_path(i1, j1, i2, j2):
-    i, j = i1, j1
-    while i1 != i2 and j1
+import numpy as np
+
+
+def paths(row1, col1, row2, col2):
+    num_right = col2 - col1
+    num_down = row2 - row1
+    return [num_right, num_down]  if num_right >= 0 and num_down >= 0 else [ None, None]
+
+
+def permute(xs, low=0):
+    if low + 1 >= len(xs):
+        yield xs
+    else:
+        for p in permute(xs, low + 1):
+            yield p
+        for i in range(low + 1, len(xs)):
+            xs[low], xs[i] = xs[i], xs[low]
+            for p in permute(xs, low + 1):
+                yield p
+            xs[low], xs[i] = xs[i], xs[low]
+
+
+class Node:
+    # MATRIX_FILE='./temp/example_p081_matrix.txt'
+    MATRIX_FILE = './temp/p081_matrix.txt'
+    matrix = np.loadtxt(MATRIX_FILE, delimiter=',')
+
+    def __init__(self, row, column, matrix=matrix):
+        self.row = row
+        self.column = column
+        self.val = self.get_val()
+        self.matrix = matrix
+
+    def down(self):
+        return Node(self.row+1, self.column)
+
+    def right(self):
+        return Node(self.row, self.column+1)
+
+    def get_val(self):
+        return self.matrix[self.row, self.column]
+
+
+def find_cost(_path):
+     cost = 0
+
+     row, col = 0, 0
+     node = Node(row, col)
+     for elem in _path:
+        cost += node.val
+        if elem == 1:
+            # Down
+            node = node.down()
+        else:
+            # Right
+            node = node.right()
+     cost += node.val
+     return cost
 
 
 def eid_081():
@@ -19,12 +74,29 @@ def eid_081():
     right(i, j) --> (i, j+1)
 
     """
+    strt_i, strt_j = 0, 0
+    end_i, end_j = 79, 79
 
-    import numpy as np
-
-    arr = np.loadtxt('./temp/p081_matrix.txt', delimiter=',')
-    init = 1
-
-    i, j = init, init
+    num_right, num_left = paths(strt_i, strt_j, end_i, end_j)
 
 
+    # 1 ==> Down
+    # 0 ==> Right
+
+    path_template = np.zeros( (2*(end_i - strt_i),), dtype=int )
+    for i in range(num_right):
+        path_template[i] = 1
+
+    # 1 ==> Down
+    # 0 ==> Right
+    path_template = np.ndarray.tolist(path_template)
+    all_paths = permute(path_template)
+    arr_path_cost = []
+
+    idx = 0
+    for path in all_paths:
+        idx+=1
+        cost = find_cost(path)
+        arr_path_cost.append(cost)
+
+    return min(arr_path_cost), "num paths:", len(arr_path_cost)
